4eda47d9917b708ff5d6e28c0c1055862e52346f
Vinicius Wille <viniciuswillealves@gmail.com>
2023-11-13 19:52:43 -0300

    first commit

---
+++ src/main/java/com/uniconnect/UniconnectApplication.java
@@ -0,0 +1,13 @@
+package com.uniconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniconnect/UniconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
